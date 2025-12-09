df35c7d5d7a5c5fb03890cf769623289c5cb9e23
Sivaram V <sivaram.v@grasko.com>
2025-12-09 14:53:24 +0530

    updated cardWrapper to handle a title and a custom button as optional props

--- components/common/cardWrapper.tsx
+++ components/common/cardWrapper.tsx
@@ -2,13 +2,28 @@ import { cn } from "@/lib/utils";
 
 const CardWrapper = ({
   children,
+  outerClassName,
   className,
+  title,
+  customBtn,
 }: {
   children: React.ReactNode;
+  outerClassName?: string;
   className?: string;
+  title?: string;
+  customBtn?: React.ReactNode;
 }) => {
   return (
-    <div className={cn("p-2 bg-background rounded-lg border shadow-md", className)}>{children}</div>
+    <div className={cn("relative rounded-lg border shadow-md bg-background", outerClassName)}>
+      {!!title && (
+        <h2 className="h-[38px] bg-secondary text-primary flex justify-center items-center text-lg font-bold rounded-t-md border border-b-primary">
+          {title}
+        </h2>
+      )}
+      {!!customBtn && <div className="absolute top-0.5 right-0.5">{customBtn}</div>}
+
+      <div className={cn("p-2 rounded-b-lg", !title && "rounded-lg", className)}>{children}</div>
+    </div>
   );
 };
 
