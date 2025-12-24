565aa22bb3f25236d8945248acb924c91bc7d189
Sivaram V <sivaram.v@grasko.com>
2025-12-24 09:48:31 +0530

    created a dueBadge and priorityConfig util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,7 @@
+import { PriorityLevel } from "@/stores/use-todo-store";
 import { clsx, type ClassValue } from "clsx";
+import { format, isPast, isToday, isTomorrow } from "date-fns";
+import { AlertCircle, ArrowDown, Flame } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -25,3 +28,59 @@ export const CARD_COLORS = [
 ];
 
 export const getRandomCardColor = () => CARD_COLORS[Math.floor(Math.random() * CARD_COLORS.length)];
+
+export const priorityConfig: Record<
+  PriorityLevel,
+  {
+    label: string;
+    icon: React.ElementType;
+    badge: string;
+    radio: string;
+  }
+> = {
+  low: {
+    label: "Low",
+    icon: ArrowDown,
+    badge: "bg-sky-100 text-sky-700 border-sky-300",
+    radio: "bg-sky-50 text-sky-800 border-sky-400",
+  },
+  medium: {
+    label: "Medium",
+    icon: AlertCircle,
+    badge: "bg-amber-100 text-amber-800 border-amber-300",
+    radio: "bg-amber-50 text-amber-900 border-amber-400",
+  },
+  high: {
+    label: "High",
+    icon: Flame,
+    badge: "bg-rose-100 text-rose-800 border-rose-300",
+    radio: "bg-rose-50 text-rose-900 border-rose-400",
+  },
+};
+
+export const getDueBadge = (date: Date, isCompleted: boolean) => {
+  if (isCompleted) return null;
+
+  if (isToday(date)) {
+    return {
+      label: "Today",
+      className: "bg-indigo-100 text-indigo-700 border border-indigo-300",
+    };
+  }
+
+  if (isTomorrow(date)) {
+    return {
+      label: "Tomorrow",
+      className: "bg-violet-100 text-violet-700 border border-violet-300",
+    };
+  }
+
+  if (isPast(date)) {
+    return {
+      label: `Overdue • ${format(date, "dd MMM yy")}`,
+      className: "bg-red-100 text-red-700 border border-red-300",
+    };
+  }
+
+  return null;
+};
